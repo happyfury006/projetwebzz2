0029f9c2ccb3ccc3b23e4bc3930ca990be8070ca
Jocelin3 <calas.jocelin@sfr.fr>
2024-11-17 23:13:37 +0100

    ajout de la page d'accueil et gestion du choix catégorie

--- index.php
+++ index.php
@@ -38,21 +38,120 @@
     $db = mysqli_connect('localhost', 'jeux-videos', 'IsImA_ZZ2/%', 'jeux-videos', 3307)
     or die('Erreur SQL'.mysqli_error($db));
     $db->query('SET NAMES UTF8');
-    $cate = $_GET['famille'];
-    $sql = "SELECT libelle, detail, prix_ttc, image FROM article WHERE id_categorie=$cate";
-    $res = $db->query($sql);
+	$sql2 = "SELECT image, libelle FROM categorie";
+	$res2 = $db -> query($sql2);
 
-    while ($data = mysqli_fetch_array($res)) {
-    ?>
-        <div class="game-card">
-            <img src="<?php echo 'images_articles/' . htmlspecialchars($data['image']); ?>" alt="Image de <?php echo htmlspecialchars($data['libelle']); ?>" class="game-image">
-            <div class="game-info">
-                <h3 class="game-title"><?php echo $data['libelle']; ?></h3>
-                <p class="game-description"><?php echo $data['detail']; ?></p>
-                <p class="game-price"><?php echo $data['prix_ttc']; ?> €</p>
-                <button class="game-button">Commander</button>
-            </div>
-        </div>
+	if (isset($_GET['famille'])){
+		$cate = $_GET['famille'];
+    	$sql = "SELECT libelle, detail, prix_ttc, image FROM article WHERE id_categorie=$cate";
+    	$res = $db->query($sql);
+		while ($data = mysqli_fetch_array($res)) {
+			?>
+				<div class="game-card">
+					<img src="<?php echo 'images_articles/' . htmlspecialchars($data['image']); ?>" alt="Image de <?php echo htmlspecialchars($data['libelle']); ?>" class="game-image">
+					<div class="game-info">
+						<h3 class="game-title"><?php echo $data['libelle']; ?></h3>
+						<p class="game-description"><?php echo $data['detail']; ?></p>
+						<p class="game-price"><?php echo $data['prix_ttc']; ?> €</p>
+						<button class="game-button">Commander</button>
+					</div>
+				</div>
+				<?php
+		}
+	}else{
+		?>
+
+		<div class="categorie">
+						<?php
+							$i=1;
+							while($data1 = mysqli_fetch_array($res2)){
+								
+
+								switch ($i){
+									case 1:
+										?>
+										<div class="unecate">
+										<a href="http://projetwebzz2/index.php?famille=1">
+											<img class="imgcate" src="<?php echo 'images_categorie/'.htmlspecialchars($data1['image']); ?>" alt="Image de  <?php echo htmlspecialchars($data1['libelle']); ?>">
+										</a>
+										<div class="nomcate">
+										<?php
+											echo 'Jeux de '.$data1['libelle'];
+										?>	
+										</div>
+										</div>
+										<?php	
+										break;
+									case 2:
+										?>
+										<div class="unecate">
+										<a href="http://projetwebzz2/index.php?famille=2">
+											<img class="imgcate" src="<?php echo 'images_categorie/'.htmlspecialchars($data1['image']); ?>" alt="Image de  <?php echo htmlspecialchars($data1['libelle']); ?>">
+										</a>
+										<div class="nomcate">
+										<?php
+											echo 'Jeux de '.$data1['libelle'];
+										?>
+										</div>
+										</div>
+										<?php
+										break;
+										
+									case 3:
+										?>
+										<div class="unecate">
+										<a href="http://projetwebzz2/index.php?famille=3">
+											<img class="imgcate" src="<?php echo 'images_categorie/'.htmlspecialchars($data1['image']); ?>" alt="Image de  <?php echo htmlspecialchars($data1['libelle']); ?>">
+										</a>
+										<div class="nomcate">
+										<?php
+											echo 'Jeux de '.$data1['libelle'];
+										?>
+										</div>
+										</div>
+										<?php
+										break;
+										
+									case 4:
+										?>
+										<div class="unecate">
+										<a href="http://projetwebzz2/index.php?famille=4">
+											<img class="imgcate" src="<?php echo 'images_categorie/'.htmlspecialchars($data1['image']); ?>" alt="Image de  <?php echo htmlspecialchars($data1['libelle']); ?>">
+										</a>
+										<div class="nomcate">
+										<?php
+											echo 'Jeux de '.$data1['libelle'];
+										?>
+										</div>
+										</div>
+										<?php
+										break;
+									case 5:
+										?>
+										<div class="unecate">
+										<a href="http://projetwebzz2/index.php?famille=5">
+											<img class="imgcate" src="<?php echo 'images_categorie/'.htmlspecialchars($data1['image']); ?>" alt="Image de  <?php echo htmlspecialchars($data1['libelle']); ?>">
+										</a>
+										<div class="nomcate">
+										<?php
+											echo 'Jeux d '.$data1['libelle'];
+										?>
+										</div>
+										</div>
+										<?php	
+									
+										break;
+
+								}	
+								$i++;
+								
+								?>
+					<?php
+					}
+				?>
+				</div>
+    
+    
     <?php
     }
     mysqli_close($db);
